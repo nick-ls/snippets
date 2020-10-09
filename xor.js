77adac17ded9177f96f4c300c02ce8f701959cf8
Nick <nick-ls@users.noreply.github.com>
2020-10-08 20:49:49 -0700

    Add documentation to xor.js

--- xor.js
+++ xor.js
@@ -1,3 +1,8 @@
+/**
+ * Performs the exclusive or operation on two integers and return the resulting value
+ * @param {int} a - The first integer
+ * @param {int} b - The second integer (order doesn't matter)
+ */
 function xor(a, b) {
 	[a, b] = [a, b].map(x=>x.toString(2)).sort((a,b)=>a.length-b.length).map(x=>x.split(""));
 	a = a.join("").padStart(b.length, "0").split("");
