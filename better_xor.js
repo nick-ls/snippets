efaa4274aa559b0ac1b2c197f5450864e15f6954
Nick <nick-ls@users.noreply.github.com>
2020-10-10 21:07:55 -0700

    Create better_xor.js
    Javascript has an xor operator that I forgot about

---
+++ better_xor.js
@@ -0,0 +1,8 @@
+/**
+ * Performs the javascript xor operation on two numbers and returns the result (using built in xor operator)
+ * @param {Number, BigInt} a - The first number
+ * @param {Number, BigInt} b - The second number
+ */
+function xor(a, b) {
+    return a ^ b;
+}
